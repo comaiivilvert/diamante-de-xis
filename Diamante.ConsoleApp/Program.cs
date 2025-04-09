60b3cb057e1334a94e89f7c526247c77c03120de
comaiivilvert <comaiivilvert@gmail.com>
2025-04-08 21:19:21 -0300

    Add project files.

---
+++ Diamante.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Diamante.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos desenhar um Diamante!");
+            Console.WriteLine("---------------------------------------------------------------");
+            Console.WriteLine("Para isso, vamos precisar que digite um numero inteiro e ímpar:");
+            
+            int numeroDiamante = int.Parse(Console.ReadLine());
+
+            while(true)
+            {
+                if (numeroDiamante % 2 != 0)
+                {
+                    Console.WriteLine("OK!! Número ímpar!!");
+
+                }
+                else
+                {
+                    Console.WriteLine("Numero invalido!! Digite um novo número ímpar: ");
+                    numeroDiamante = int.Parse(Console.ReadLine());
+                }
+                
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
