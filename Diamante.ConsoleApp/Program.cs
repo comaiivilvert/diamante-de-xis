532abe2ec69e298531181baae98ea12092caef06
comaiivilvert <comaiivilvert@gmail.com>
2025-04-09 00:10:05 -0300

    implementado a parte de cima do diamante + linha central.

--- Diamante.ConsoleApp/Program.cs
+++ Diamante.ConsoleApp/Program.cs
@@ -9,15 +9,15 @@
             Console.WriteLine("---------------------------------------------------------------");
             Console.WriteLine("Para isso, vamos precisar que digite um numero inteiro e ímpar:");
             Console.WriteLine("---------------------------------------------------------------");
-            int numeroDiamante = Convert.ToInt32(Console.ReadLine());
-
-            int numeroImpar = ValidaSeEhImpar(numeroDiamante);
            
-            Console.ReadLine();   
-
+            int numeroDiamante = Convert.ToInt32(Console.ReadLine());
+            int tamanhoDiamante = ValidaSeEhImpar(numeroDiamante);
+            
+            Console.Clear();
+            Console.WriteLine($"Aqui está seu diamante de tamanho: {tamanhoDiamante}");
+            DesenhaDiamante(tamanhoDiamante);
+            Console.ReadLine();
         }
-
-
 
         static int ValidaSeEhImpar(int numeroDiamante)
         {
@@ -30,5 +30,37 @@
             return numeroDiamante;
             
         }
+
+        static void DesenhaDiamante(int tamanhoDiamante)
+        {
+
+            int metade = tamanhoDiamante / 2;
+
+            //parte de cima do diamente (incluindo linha central)
+            for (int i = 0; i <= metade; i++)
+            {
+                int espacos = metade - i;
+                int asteriscos = i * 2 + 1;
+
+                //imprime os espaços em branco
+                for (int j = 0; j < espacos; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                //imprime os asteriscos *
+                for (int j = 0; j < asteriscos; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+           
+
+
+        }
     }
 }
+    
