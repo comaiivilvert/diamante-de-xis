aa5a01d6c6cc6e46e30b34628cca186fb6ffe791
comaiivilvert <comaiivilvert@gmail.com>
2025-04-18 18:37:20 -0300

    estruturado com a classe separada para desenhar o diamante.

---
+++ Diamante.ConsoleApp/DesenhaDiamante.cs
@@ -0,0 +1,55 @@
+﻿namespace Diamante.ConsoleApp
+{
+    internal class DesenhaDiamante
+    {
+        public static void DesenhaDiamanteInteiro(int tamanhoDiamante)
+        {
+
+            int metade = tamanhoDiamante / 2;
+
+            //parte de cima do diamente (incluindo linha central)
+            for (int i = 0; i <= metade; i++)
+            {
+                int espacos = metade - i;
+                int asteriscos = i * 2 + 1;
+
+                //imprime os espaços em branco
+                for (int j = 0; j < espacos; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                //imprime os asteriscos *
+                for (int j = 0; j < asteriscos; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            //parte de baixo do diamante
+            for (int i = metade - 1; i >= 0; i--)
+            {
+                int espacos = metade - i;
+                int asteriscos = i * 2 + 1;
+
+                //imprime os espaços em branco
+                for (int j = 0; j < espacos; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                //imprime os asteriscos *
+                for (int j = 0; j < asteriscos; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
--- Diamante.ConsoleApp/Program.cs
+++ Diamante.ConsoleApp/Program.cs
@@ -15,7 +15,9 @@
             
             Console.Clear();
             Console.WriteLine($"Aqui está seu diamante de tamanho: {tamanhoDiamante}");
-            DesenhaDiamante(tamanhoDiamante);
+            
+            DesenhaDiamante.DesenhaDiamanteInteiro(tamanhoDiamante);
+            
             Console.ReadLine();
         }
 
@@ -31,55 +33,6 @@
             
         }
 
-        static void DesenhaDiamante(int tamanhoDiamante)
-        {
-
-            int metade = tamanhoDiamante / 2;
-
-            //parte de cima do diamente (incluindo linha central)
-            for (int i = 0; i <= metade; i++)
-            {
-                int espacos = metade - i;
-                int asteriscos = i * 2 + 1;
-
-                //imprime os espaços em branco
-                for (int j = 0; j < espacos; j++)
-                {
-                    Console.Write(" ");
-                }
-
-                //imprime os asteriscos *
-                for (int j = 0; j < asteriscos; j++)
-                {
-                    Console.Write("*");
-                }
-
-                Console.WriteLine();
-            }
-
-            //parte de baixo do diamante
-            for (int i = metade -1; i >= 0; i--)
-            {
-                int espacos = metade - i;
-                int asteriscos = i * 2 + 1;
-
-                //imprime os espaços em branco
-                for (int j = 0; j < espacos; j++)
-                {
-                    Console.Write(" ");
-                }
-
-                //imprime os asteriscos *
-                for (int j = 0; j < asteriscos; j++)
-                {
-                    Console.Write("*");
-                }
-
-                Console.WriteLine();
-            }
-
-
-        }
     }
 }
     
